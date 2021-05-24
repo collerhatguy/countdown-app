f602bf5b30c4efa9dc76b2b20e34394d6b63f680
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-05-24 12:46:19 -0400

    added text styling and footer images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import CountDownList from "./components/CountDownList";
+import facebookImage from "./images/icon-facebook.svg";
+import instagramImage from "./images/icon-instagram.svg";
+import pinterestImage from "./images/icon-pinterest.svg";
 import "./styles/styles.css";
 
 function App() {
   return (
     <div className="App">
       <CountDownList />
+      <footer>
+        <img src={facebookImage} />
+        <img src={instagramImage} />
+        <img src={pinterestImage} />
+      </footer>
     </div>
   );
 }
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,7 +3,10 @@ import React from 'react'
 export default function Counter({timeUnit}) {
     return (
         <div className="counter">
-            
+            <div className="time-box">
+
+            </div>
+            <div className="time-unit">{timeUnit}</div>
         </div>
     )
 }
