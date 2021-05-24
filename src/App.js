4f80be70c3b060bf5d6560e1d3f38d4313db1660
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-05-24 12:09:14 -0400

    added basic template

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div className="App">
+      Frontend Mentor | Launch countdown timer
+
+    We're launching soon Days Hours Minutes Seconds
+    Challenge by Frontend Mentor. Coded by Your Name Here. 
+    
+    </div>
+  );
+}
+
+export default App;
