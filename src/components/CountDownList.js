dd5eafc85f46e1b4241cb7b17f2052c640615352
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-05-25 13:56:25 -0400

    added timer functionality

--- src/components/CountDownList.js
+++ src/components/CountDownList.js
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Counter from "./Counter";
 
 export default function CountDownList() {
+    const futureDate = new Date("Jun 25, 2021 16:37:52").getTime();
+    const [currentDate, setCurrentDate] = useState(new Date().getTime());
+    const [timeLeft, setTimeLeft] = useState(futureDate - currentDate);
+    const [daysLeft, setDaysLeft] = useState(Math.floor(timeLeft / (1000 * 60 * 60 * 24)));
+    const [hoursLeft, setHoursLeft] = useState(Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    const [minutesLeft, setMinutesLeft] = useState(Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60)))
+    const [secondsLeft, setSecondsLeft] = useState(Math.floor((timeLeft % (1000 * 60)) / 1000))
+    const [secondPassed, setSecondPassed] = useState(0);
+    setInterval(() => {
+        setSecondPassed(secondPassed + 1);
+    }, 1000)
+    useEffect(() => {
+        setCurrentDate(new Date().getTime())
+        setTimeLeft(futureDate - currentDate)
+        setDaysLeft(Math.floor(timeLeft / (1000 * 60 * 60 * 24)));
+        setHoursLeft(Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+        setMinutesLeft(Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60)));
+        setSecondsLeft(Math.floor((timeLeft % (1000 * 60)) / 1000))
+    }, [secondPassed])
+
     return (
         <div id="countdown-container">
-            <Counter timeUnit={"Days"}/>
-            <Counter timeUnit={"Hours"}/>
-            <Counter timeUnit={"Minutes"}/>
-            <Counter timeUnit={"Seconds"}/>
+            <Counter 
+                timeUnit={"Days"} 
+                value={daysLeft}
+            />
+            <Counter 
+                timeUnit={"Hours"} 
+                value={hoursLeft} 
+            />
+            <Counter 
+                timeUnit={"Minutes"} 
+                value={minutesLeft}
+            />
+            <Counter 
+                timeUnit={"Seconds"} 
+                value={secondsLeft}
+            />
         </div>
     )
 }
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default function Counter({timeUnit}) {
+export default function Counter({timeUnit, value}) {
     return (
         <div className="counter">
             <div className="time-box">
-
+                {value}
             </div>
             <div className="time-unit">{timeUnit}</div>
         </div>
